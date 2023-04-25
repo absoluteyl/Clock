4dffa853f8838a3e10eee3cf0425468f9a1529a0
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-25 17:46:54 +0800

    add implementation in basic proxy
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- src/BasicProxy.sol
+++ src/BasicProxy.sol
@@ -5,4 +5,13 @@ import { Proxy } from "./Proxy.sol";
 
 contract BasicProxy is Proxy {
   // First slot is the address of the current implementation
+  address public implementation;
+
+  function __Proxy_init(address _implementation) public {
+    implementation = _implementation;
+  }
+
+  fallback() external {
+    _delegate(implementation);
+  }
 }
